22a6e5c0715a171cc626ed70f9a0b074e70a571b
mark <bambooratmark@gmail.com>
2021-07-13 15:28:45 +0800

    測試資料新增 memo

--- MD.js
+++ MD.js
@@ -13,6 +13,7 @@ p25_cny_tax_id_number	統一編號	必填,最長124字元
 p25_cny_principal_name	負責人姓名	必填,最長124字元	
 p25_cny_principal_phone	負責人電話	必填,最長124字元	
 p25_cny_register_address	登記地址	必填,最長124字元	
-p25_cny_business_address	營業地址	必填,最長124字元	
+p25_cny_business_address	營業地址備註	必填,最長124字元	
+p25_cny_business_memo	營業地址	必填,最長124字元	
 p25_cny_memo	備註	最長124字元
 `;
