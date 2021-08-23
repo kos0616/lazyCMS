038ea9a22e9ef0180247e4a3b6ff73e8bbadb140
mark <javert0616@gmail.com>
2021-08-23 18:00:45 +0800

    準備分離權限與 API

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,7 @@
 /** 取得文件轉換過來的值 */
 const inputs = require("./lib/fileReader");
-const API = inputs.API.trim();
+const API = inputs.API_PATH.trim();
+const API_AUTH = inputs.API_AUTH.trim();
 const form = inputs.settedForm;
 const { key, NODE } = inputs;
 const folder = API.split("/")[1]; /* 過濾出 folder 的路徑 */
@@ -22,23 +23,28 @@ const FOLDER = {
 
     if (data.confirm === false)
       throw new Error("使用者確認欄位錯誤，中斷操作!");
+    const res = { API, API_AUTH, ...data, folder, key, form, NODE };
 
-    const res = { API, ...data, folder, key, form, NODE };
-
-    const actions = [...Service(res),...Core(res), ...setTheme(data.themeType, res)];
+    const actions = [
+      ...Service(res),
+      ...Core(res),
+      ...setTheme(data.themeType, res),
+    ];
     return actions;
   },
 };
 
-const inputBlock =  require('./plop-templates/view/inputBlock')
+const inputBlock = require("./plop-templates/view/inputBlock");
 
 module.exports = function (plop) {
   /** 判定是否可用 el-switch */
-  plop.setHelper('isSwitch',function (arr, opts) {
-    return (Array.isArray(arr) && arr.length === 2) ? opts.fn(this) : opts.inverse(this)
-  })
+  plop.setHelper("isSwitch", function (arr, opts) {
+    return Array.isArray(arr) && arr.length === 2
+      ? opts.fn(this)
+      : opts.inverse(this);
+  });
 
-  plop.setPartial('inputPartial', inputBlock)
+  plop.setPartial("inputPartial", inputBlock);
 
   plop.setGenerator("FOLDER", FOLDER);
 };
@@ -46,16 +52,16 @@ module.exports = function (plop) {
 /**
  * 取得樣式
  * @param {'Basic' | 'Dialog'} themeType 外觀樣式的類型
- * @returns 
+ * @returns
  */
 function setTheme(themeType, res) {
   let Theme = [];
 
-  if(themeType === 'Dialog') {
-    Theme = DialogTheme(res)
+  if (themeType === "Dialog") {
+    Theme = DialogTheme(res);
   }
-  if (themeType === 'Basic') {
-    Theme = BasicTheme(res)
+  if (themeType === "Basic") {
+    Theme = BasicTheme(res);
   }
   return Theme;
 }
